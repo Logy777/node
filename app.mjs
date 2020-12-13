a8c71db9a1d3197a23df65a33d8459cd4e3c1156
Dzianis Zavoiskikh <6557587@gmail.com>
2020-12-13 22:25:08 +0300

    pipeline inside a repo

--- app.mjs
+++ app.mjs
@@ -5,7 +5,7 @@ import connectToDatabase from './helpers.mjs'
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h2>Hi there! <br> The deployment works again! <br> And Now its starting with a webhook!</h2>');
+  res.send('<h2>Hi there! <br> The deployment works again! <br> And Now its starting with a webhook! <br> Pipeline executed from SCM repo!</h2>');
 });
 
 await connectToDatabase();
